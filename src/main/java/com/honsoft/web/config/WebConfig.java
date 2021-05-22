dde0d7ab1abd56e681f581af75d4eb9fd0026cd0
changnam go <changnma.go@gmail.com>
2021-05-22 11:33:51 +0900

    initial commit

---
+++ src/main/java/com/honsoft/web/config/WebConfig.java
@@ -0,0 +1,44 @@
+package com.honsoft.web.config;
+
+import javax.servlet.Filter;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.honsoft.web.filter.MyFilter;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+	
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		registry.jsp("/WEB-INF/jsp/", ".jsp");
+	}
+
+	// Register Filter
+	@Bean
+	public FilterRegistrationBean<MyFilter> myFilter() {
+		FilterRegistrationBean<MyFilter> filter = new FilterRegistrationBean<>(new MyFilter());
+		filter.addUrlPatterns("/*");
+		filter.setOrder(3);
+		// Mapping filter to a Servlet
+		// bean.addServletRegistrationBeans(new ServletRegistrationBean[] {
+		// servletRegistrationBean()
+		// });
+		return filter;
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("index");
+		registry.addViewController("/home").setViewName("thymeleaf/home");
+	}
+
+}
